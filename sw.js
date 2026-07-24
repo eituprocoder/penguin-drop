7b5ded102ea4b6a484af2d2bbf74699213459497
Eituprocoder <eitupro256@gmail.com>
2026-07-24 19:57:14 +0300

    Rename cache from 'Penguin-drop-game' to 'Penguin-drop'

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. Cambia el número de versión cada vez que modifiques tu HTML u otros archivos
-const cacheName = 'Penguin-drop-game'; 
+const cacheName = 'Penguin-drop'; 
 
 const assets = [
   './',
